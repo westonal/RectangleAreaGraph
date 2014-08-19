b447083387bc588d2dbc342ffa8f50accf305e00
Alan Evans <thealanevans@googlemail.com>
2014-08-19 06:32:56 +0100

    Example project.

---
+++ RectAreaPngExample/src/com/coltsoftware/example/ValuesToPng.java
@@ -0,0 +1,13 @@
+package com.coltsoftware.example;
+
+public class ValuesToPng {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
