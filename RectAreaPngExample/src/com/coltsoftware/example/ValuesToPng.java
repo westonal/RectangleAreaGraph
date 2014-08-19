1b51d6861e5a055be5b62e32421607549e783c8e
Alan Evans <thealanevans@googlemail.com>
2014-08-19 06:53:17 +0100

    Working example, showing how to take list of values and represent as area rectangle graph.

--- RectAreaPngExample/src/com/coltsoftware/example/ValuesToPng.java
+++ RectAreaPngExample/src/com/coltsoftware/example/ValuesToPng.java
@@ -1,13 +1,69 @@
 package com.coltsoftware.example;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Paint;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import com.coltsoftware.rectangleareagraph.Rectangle;
+import com.coltsoftware.rectangleareagraph.RectangleSplit;
+import com.coltsoftware.rectangleareagraph.RectangleSplit.SplitResult;
+
 public class ValuesToPng {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RectangleSplit rectangleSplit = new RectangleSplit();
+		rectangleSplit.addValue(7, "A");
+		rectangleSplit.addValue(8, "B");
+		rectangleSplit.addValue(9, "C");
+		rectangleSplit.addValue(10, "D");
+		rectangleSplit.addValue(30, "E");
+		rectangleSplit.addValue(40, "F");
+		rectangleSplit.addValue(60, "G");
+		rectangleSplit.addValue(80, "H");
+		rectangleSplit.addValue(130, "I");
+		int width = 800;
+		int height = 640;
+		List<SplitResult> split = rectangleSplit.split(new Rectangle(0, 0,
+				width - 1, height - 1));
+		RenderedImage image = render(split, width, height);
+		saveImage(image);
+	}
 
+	private static RenderedImage render(List<SplitResult> split, int width,
+			int height) {
+		BufferedImage bufferedImage = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = bufferedImage.createGraphics();
+		Paint p = Color.BLACK;
+		graphics.setPaint(p);
+		for (SplitResult result : split) {
+			Rectangle rectangle = result.getRectangle();
+
+			graphics.drawRect(rectangle.getLeft(), rectangle.getTop(),
+					rectangle.getWidth(), rectangle.getHeight());
+			graphics.drawChars(result.getDescription().toCharArray(), 0, result
+					.getDescription().length(),
+					rectangle.getLeft() + rectangle.getWidth() / 2,
+					rectangle.getTop() + rectangle.getHeight() / 2);
+		}
+		return bufferedImage;
+	}
+
+	private static void saveImage(RenderedImage bufferedImage) {
+		try {
+			File outputfile = new File("saved.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
